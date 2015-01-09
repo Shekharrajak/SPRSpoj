7cf45526f34f1176dc295764ce7cf85fa3057950
Shekhar Prasad Rajak <shekharstudy@ymail.com>
2015-01-10 02:42:34 +0530

    added

--- SPOJ ME/TOANDFRO1.cpp
+++ SPOJ ME/TOANDFRO1.cpp
@@ -13,7 +13,7 @@ int main(int argc, char** argv) {
 	int k=0;char arr[200][100];
 	int len=strlen(a)/n;
 	for( int i=0;i<len;i++){
-		if(i%2!=0){
+		if(i%2==0){
 			for(int j=0;j<n;j++)
 				arr[i][j] =a[k++];
 		}
@@ -32,3 +32,42 @@ int main(int argc, char** argv) {
 }
 	return 0;
 }
+/*
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t)
+    {
+        char arr[300];
+        scanf("%s",arr);
+        int len=strlen(arr)/t;
+        char ans[len][t];
+        int i,j,k=0;
+        for(i=0;i<len;i++)
+        {
+            if(i%2)
+            {
+                for(j=t-1;j>=0;j--)
+                   ans[i][j]=arr[k++];
+            }
+            else{
+            for(j=0;j<t;j++)
+            {
+                ans[i][j]=arr[k++];
+            }}
+        }
+        for(i=0;i<t;i++)
+        {
+            for(j=0;j<len;j++)
+                printf("%c",ans[j][i]);
+        }
+        printf("\n");
+        scanf("%d",&t);
+    }
+    return 0;
+}
+
+*/
