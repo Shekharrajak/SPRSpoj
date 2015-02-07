9b65ea40db91a80f59c16166607a262a1c956fb9
Shekhar Prasad Rajak <shekharstudy@ymail.com>
2015-02-07 12:28:47 +0530

    added

---
+++ SPOJ ME/scpc11b.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include<iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n, i;
+	scanf("%d\n", &n);
+	//bool c = b==1;
+	
+/*	while(scanf("%d", &n) && n) {
+		{
+		
+		for(i = 0; i < n; i++) scanf("%d", a + i);
+		sort(a, a + n);
+		for(i = 1; i < n; i++) {
+			if(a[i] - a[i-1] > 200)
+				 break;
+		}
+		if(i < n || (1422 - a[i-1])*2 > 200) printf("IMPOSSIBLE");
+		else printf("POSSIBLE");
+		}
+	}
+	*/
+	return 0;
+}
+
+
+
+
+
