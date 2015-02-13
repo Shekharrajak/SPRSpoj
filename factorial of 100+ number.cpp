a03ee3ae4749e455d56447d54d05bc7576569fba
Shekhar Prasad Rajak <shekharstudy@ymail.com>
2015-02-13 12:40:03 +0530

    Update factorial of 100+ number.cpp

--- factorial of 100+ number.cpp
+++ factorial of 100+ number.cpp
@@ -8,18 +8,18 @@ printf("Enter the number\n");
 scanf("%d",&n);
 for(;n>=2;n--)
 {
-temp=0;
-for(i=0;i<=index;i++)
-{
-temp=(a[i]*n)+temp;
-a[i]=temp%10;
-temp=temp/10;
-}
-while(temp>0)
-{
-a[++index]=temp%10;
-temp=temp/10;
-}
+      temp=0;
+      for(i=0;i<=index;i++)
+      {
+      temp=(a[i]*n)+temp;
+      a[i]=temp%10;
+      temp=temp/10;
+      }
+      while(temp>0)
+      {
+      a[++index]=temp%10;
+      temp=temp/10;
+      }
 }
 for(i=index;i>=0;i--)
 printf("%d",a[i]);
